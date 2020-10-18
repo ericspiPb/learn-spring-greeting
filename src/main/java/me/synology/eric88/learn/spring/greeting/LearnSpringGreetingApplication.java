11e8aa15133234833c69c9a85fa026fe22483b06
Eric Chan <ericspi@outlook.com>
2020-10-18 18:36:46 +0800

    hello world of spring

---
+++ src/main/java/me/synology/eric88/learn/spring/greeting/LearnSpringGreetingApplication.java
@@ -0,0 +1,13 @@
+package me.synology.eric88.learn.spring.greeting;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LearnSpringGreetingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LearnSpringGreetingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/me/synology/eric88/learn/spring/greeting/LearnSpringGreetingApplicationTests.java
@@ -0,0 +1,13 @@
+package me.synology.eric88.learn.spring.greeting;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LearnSpringGreetingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
